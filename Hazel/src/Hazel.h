2b48e4ff58d34281147271cff184a33bf12812a4
darter <darter@telenet.be>
2019-03-03 20:51:31 +0100

    premake added

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -2,6 +2,7 @@
 
 // for use by hazel applications
 #include "Hazel/Application.h"
+#include "Hazel/Log.h"
 
 // ------Entry Point ----------------
 #include "Hazel/EntryPoint.h"
--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -6,8 +6,14 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 
 int main() {
+	Hazel::Log::Init();
+	HZ_CORE_WARN("Initialized Log!");
+	HZ_INFO("Hello u");
+	HZ_ERROR("boooooeeeeeeem");
 	Hazel::Application* sandbox = Hazel::CreateApplication();
 	sandbox->Run();
 	delete sandbox;
 }
+
+
 
